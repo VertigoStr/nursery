c11f4b7f8d120aabd29f7d9cf8ec740e14d38a33
ramil <ramil@idpowers.com>
2018-06-30 16:02:23 +0300

    feat(api.v1.journal): проверка того, что ребенка забрал нужный родитель

--- server/api/v1/journal/serializers.py
+++ server/api/v1/journal/serializers.py
@@ -1,3 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 
 from apps.journal.models import Journal
@@ -11,3 +14,12 @@ class JournalSerializer(ModelSerializer):
             'id', 'child', 'time',
             'action', 'parent', 'date'
         ]
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        child = data.get('child') or self.instance.child
+        parent = data.get('parent') or self.instance.parent
+        if child and parent and not child.parents.filter(id=parent.id).exists():
+            raise ValidationError({'parent': _('Указанный родитель не является '
+                                               'родителем выбранного ребенка!')})
+        return data
